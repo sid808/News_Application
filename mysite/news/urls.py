4585cb3296cd88451c270d7bac7a75eb717f5ada
Siddharth Aadarsh <siddharth.aadarsh@healthify.co>
2021-06-18 20:48:45 +0530

    Adding migrations and improving views, urls

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -1,14 +1,15 @@
 from news import views
 from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = [
     # http://localhost:8000/news/articles
-    url(r'articles', views.index, name='index'),
+    path(r'articles', views.index, name='index'),
     # http://localhost:8000/news/articles/5
-    url(r'articles/<int:year>', views.year_archive, name='year_archive'),
+    path(r'articles/<int:year>', views.year_archive, name='year_archive'),
+    
+    path(r'articles/<int:year>/<str:month>', views.month_archive, name='month_archive'),
 
-    url(r'articles/<int:year>/<int:month>', views.month_archive, name='month_archive'),
-
-    url(r'articles/<int:year>/<int:month>/<int:pk>', views.article_archive, name='article_archive'),
+    path(r'articles/<int:year>/<str:month>/<int:pk>', views.article_archive, name='article_archive'),
 ]
 
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -10,14 +10,14 @@ def year_archive(request, year):
     # a_list = Article.objects.filter(pub_date__year=year)
     # context = {'year': year, 'article_list': a_list}
     # return render(request, 'news/year_archive.html', context)
-    return HttpResponse("Hello, world. You are at the YEAR archieve.")
+    return HttpResponse("Hello, world. You are at the %s YEAR archieve." % year)
 
 
 def month_archive(request, year, month):
 
-    return HttpResponse("Hello, world. You are at the MONTH archieve.")
+    return HttpResponse("Hello, world. You are at the %s YEAR %s MONTH archieve." % (year, month))
 
 
 def article_archive(request, year, month, pk):
 
-    return HttpResponse("Hello, world. You are at the ARTICLE archieve.")
+    return HttpResponse("Hello, world. You are at the %s YEAR %s MONTH ARCHIVE: %s." % (year, month, pk))
